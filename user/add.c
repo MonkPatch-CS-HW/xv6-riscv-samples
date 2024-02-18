600e88b072e8606704f30797cbb0f8438ce6f9db
MonkPatch <monkpatch@gmail.com>
2024-02-18 11:54:29 +0300

    Improve error handling

--- user/add.c
+++ user/add.c
@@ -10,9 +10,20 @@ main(int argc, char *argv[])
   char buf[BUF_SIZE];
   gets(buf, BUF_SIZE);
 
-  char *space = strchr(buf, ' ');
+  char *space = 0;
+  int len = strlen(buf);
+  for (int i = 0; i < len - 1; i++) {
+    if (buf[i] == ' ') {
+      if (i == 0 || i == len - 1 || space != 0) 
+        goto err_space;
+      space = buf + i;
+    } else if (buf[i] < '0' || buf[i] > '9') {
+      goto err_char;
+    }
+  }
+
   if (space == 0)
-    goto fmt_err;
+    goto err_space;
 
   char *buf_a = buf;
   char *buf_b = space + 1;
@@ -20,11 +31,15 @@ main(int argc, char *argv[])
   int a = atoi(buf_a);
   int b = atoi(buf_b);
 
-  printf("%d\n", a + b);
+  printf("result: %d\n", a + b);
 
   exit(0);
 
-fmt_err:
-  printf("two numbers needed\n");
+err_space:
+  fprintf(2, "need exactly one space between numbers\n");
+  exit(1);
+
+err_char:
+  fprintf(2, "incorrect numbers\n");
   exit(1);
 }
